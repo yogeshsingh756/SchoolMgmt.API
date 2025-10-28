a5d89666e335a4735bc14a20a2662b1fc9f9b940
yogeshsingh756 <ythakursingh98@gmail.com>
2025-10-28 23:45:54 +0530

    fix

---
+++ SchoolMgmt.Application/DTOs/Admin/AssignSubjectRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMgmt.Application.DTOs.Admin
+{
+    public class AssignSubjectRequest
+    {
+        public int ClassId { get; set; }
+        public int? SectionId { get; set; }
+        public int SubjectId { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
