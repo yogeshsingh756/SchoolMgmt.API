7fb801e4b7f56441312a304bb667a0c25d225b61
aeroabhi1980 <77203884+aeroabhi1980@users.noreply.github.com>
2025-12-04 16:48:48 +0530

    Add new CORS origin for erp.kpmic.in

--- SchoolMgmt.API/Program.cs
+++ SchoolMgmt.API/Program.cs
@@ -128,7 +128,8 @@ builder.Services.AddCors(options =>
                 "https://sfms.abhiworld.in",   
                 "http://localhost:3000",      
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://erp.kpmic.in"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
